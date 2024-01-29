f3acb35300f7fd9bd730fb2888c5c8f1258f334d
toni8482 <toni8482@outlook.com>
2024-01-29 23:44:40 +0100

    commit

---
+++ src/main/test/java/AppTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+public class AppTest {
+
+    @Test
+    public void testApp(){
+
+        Application myApp = new Application();
+
+        String result = myApp.getStatus();
+    
+        assertEquals("OK", result);
+    }
+ 
+}
