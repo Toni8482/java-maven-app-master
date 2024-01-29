eafb23cefe922cffc427a79bb9f19cd0aaa86dbd
toni8482 <toni8482@outlook.com>
2024-01-30 00:19:20 +0100

    commit

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,17 @@
-package test.java;
+import com.example.Application;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class AppTest {
 
     @Test
-    public void testApp(){
-
+    public void testApp() {
         Application myApp = new Application();
 
         String result = myApp.getStatus();
-    
+
         assertEquals("OK", result);
     }
- 
+
 }
